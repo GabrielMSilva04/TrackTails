af8bf72a0dc5f1eaab0dcad06386160af77afa92
DJI28 <diogodomingues04@ua.pt>
2024-11-04 12:32:41 +0000

    AnimalService API is done, needs more testing though.

--- animalservice/src/main/java/ies/tracktails/animalservice/services/AnimalService.java
+++ animalservice/src/main/java/ies/tracktails/animalservice/services/AnimalService.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 public interface AnimalService {
     public Animal addAnimal(Animal animal);
-    public void removeAnimal(long id);
-    public Animal updateAnimal(long id, Animal animal);
-    public Animal getAnimal(long id);
+    public void removeAnimal(Long id);
+    public Animal updateAnimal(Long id, Animal animal);
+    public Animal getAnimal(Long id);
     public Animal getAnimalByName(String name);
     public List<Animal> getAllAnimals();
+    public List<Animal> getAnimalsByUserId(Long userId);
 }
