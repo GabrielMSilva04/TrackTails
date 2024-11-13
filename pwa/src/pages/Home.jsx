dfeb440224b7fe89ecb3a361a3565ec6765ab464
martimsantos <santos.martim@ua.pt>
2024-11-13 20:11:06 +0000

    layout for all the pages done, and add a theme to daisyUI

--- pwa/src/pages/Home.jsx
+++ pwa/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return(
-        <>
-            <p>home</p>
-        </>
+        <h2>
+            home
+        </h2>
     );
 }
--- pwa/tailwind.config.js
+++ pwa/tailwind.config.js
@@ -7,6 +7,10 @@ export default {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: ["lemonade"],
+  },
+
   plugins: [
     require('daisyui'),
   ],
