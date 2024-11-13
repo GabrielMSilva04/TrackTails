379fc2942e69eb8dbaa09f3fd5aa4531d06ecfa2
Sebastião Teixeira <steixeira2004@gmail.com>
2024-11-13 00:56:51 +0000

    Add Manifest and Service worker to pwa and fix react router implementation

--- pwa/src/components/Layout.jsx
+++ pwa/src/components/Layout.jsx
@@ -1,14 +1,23 @@
-export default function Layout({ children }) {
-    return (
-        <div>
-            <nav>
-                <ul>
-                <li><a href="/">Home</a></li>
-                </ul>
-            </nav>
-            <div className={"container"}>
-                <main>{children}</main>
-            </div>
-        </div>
-    )
+import { Outlet, Link } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
