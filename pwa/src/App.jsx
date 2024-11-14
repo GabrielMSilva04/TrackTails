96456aab3d6039668924b737c37d4ee9f1eb5784
martimsantos <santos.martim@ua.pt>
2024-11-14 02:06:26 +0000

    layoutanimal done, theme outdated

--- pwa/src/App.jsx
+++ pwa/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Layout from './components/Layout'
+import LayoutAnimal from './components/LayoutAnimal'
 import Home from './pages/Home'
 import './App.css'
 
@@ -16,6 +17,9 @@ function App() {
           {/* 404 Not Found Route */}
           <Route path="*" element={<h2>404 - Page Not Found</h2>} />
         </Route>
+          <Route path="/test" element={<LayoutAnimal />}>
+
+          </Route>
       </Routes>
     </Router>
   )
---
+++ pwa/src/components/LayoutAnimal.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from 'react-router-dom';
+
+
+export default function LayoutAnimal() {
+    return (
+        <div className="bg-primary h-screen flex flex-col">
+            <div className="w-full flex justify-center items-center mt-12">
+                <h1 className="text-3xl text-white font-bold tracking-wide">trackTails.</h1>
+            </div>
+            <div className="avatar placeholder">
+                <div className="bg-neutral border-8 border-white text-neutral-content w-32 rounded-full z-10 mx-auto mt-6">
+                    <img src="https://ilovemydogsomuch.com/wp-content/uploads/2023/02/323839-1600x1066-border-collie-breed-1400x933.jpg" alt="adsasd"/>
+                </div>
+            </div>
+            <div className="bg-primary">
+                <div className="bg-white w-full h-3/4 rounded-t-3xl p-8 flex flex-col items-center absolute bottom-0">
+                    <div className="mt-10 text-primary font-bold">
+                        Jack
+                    </div>
+                    <Outlet/>
+                </div>
+            </div>
+        </div>
+    );
+}
