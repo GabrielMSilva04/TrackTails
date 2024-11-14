cf428f3f178db51256b873ba21113108757fffb1
martimsantos <santos.martim@ua.pt>
2024-11-14 01:22:08 +0000

    add constant InputField

--- pwa/src/pages/Login.jsx
+++ pwa/src/pages/Login.jsx
@@ -1,3 +1,16 @@
+const InputField = ({ label, type, placeholder, value, onChange }) => {
+    return (
+        <div className="mb-2">
+            <label className="text-secondary text-sm font-bold">{label}</label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                className="input h-10 bg-base-200 text-primary w-full"
+            />
+        </div>
+    );
+};
+
 export default function Login() {
     return (
         <div className="bg-primary h-screen flex flex-col">
@@ -10,23 +23,21 @@ export default function Login() {
             <div className="bg-white w-full h-3/4 rounded-t-3xl p-8 flex flex-col items-center absolute bottom-0">
                 <h2 className="text-2xl font-bold text-primary mb-4">Welcome Back</h2>
                 <form className="w-3/4 mt-8 flex flex-col">
-                    <label className="text-secondary text-sm font-bold">Email</label>
-                    <input
+                    <InputField
+                        label="Email"
                         type="email"
                         placeholder="Email"
-                        className="input h-10 bg-base-200 text-primary mb-2"
                     />
-                    <label className="text-secondary text-sm font-bold">Password</label>
-                    <input
+                    <InputField
+                        label="Password"
                         type="password"
-                        placeholder="Password"
-                        className="input h-10 bg-base-200 text-primary mb-2"
+                        placeholder="********"
                     />
                     <button className="btn btn-primary text-white w-full mt-6">Log in</button>
                 </form>
                 <p className="text-sm mt-32">
                     Not have an account?{" "}
-                    <a href="#" className="text-secondary font-bold">
+                    <a href="/register" className="text-secondary font-bold">
                         Sign Up
                     </a>
                 </p>
