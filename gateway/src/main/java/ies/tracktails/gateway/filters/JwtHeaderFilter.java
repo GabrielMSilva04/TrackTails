77863169c65947fd50846ee1509ff8da3a6435b7
Sebastião Teixeira <steixeira2004@gmail.com>
2024-11-27 19:04:55 +0000

    Fix X-User-Id insertion in headers

--- gateway/src/main/java/ies/tracktails/gateway/filters/JwtHeaderFilter.java
+++ gateway/src/main/java/ies/tracktails/gateway/filters/JwtHeaderFilter.java
@@ -1,6 +1,7 @@
 package ies.tracktails.gateway.filters;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -9,26 +10,49 @@ import reactor.core.publisher.Mono;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+
 public class JwtHeaderFilter implements WebFilter {
+
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         System.out.println("JwtHeaderFilter");
+
+        // Obtém o contexto de segurança reativo
         return ReactiveSecurityContextHolder.getContext()
             .flatMap(securityContext -> {
                 System.out.println("SecurityContext: " + securityContext);
                 var authentication = securityContext.getAuthentication();
                 System.out.println("Authentication: " + authentication);
+
+                // Se o token for do tipo JWT, extraímos o 'sub' (userId)
                 if (authentication instanceof JwtAuthenticationToken) {
                     System.out.println("JwtAuthenticationToken");
                     Jwt jwt = ((JwtAuthenticationToken) authentication).getToken();
                     String userId = jwt.getSubject();
                     System.out.println("UserId: " + userId);
-                    exchange.getRequest().mutate()
-                        .header("X-User-Id", userId)
-                        .build();
+
+                    // Cria uma nova instância de HttpHeaders e adiciona o cabeçalho 'X-User-Id'
+                    HttpHeaders modifiedHeaders = new HttpHeaders();
+                    modifiedHeaders.putAll(exchange.getRequest().getHeaders());
+                    modifiedHeaders.add("X-User-Id", userId); // Adiciona o cabeçalho
+
+                    // Cria a requisição modificada com os cabeçalhos alterados
+                    ServerHttpRequestDecorator modifiedRequest = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                        @Override
+                        public HttpHeaders getHeaders() {
+                            return modifiedHeaders;  // Retorna os cabeçalhos modificados
+                        }
+                    };
+
+                    // Cria um novo ServerWebExchange com a requisição modificada
+                    ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+
+                    // Prossegue com a requisição modificada
+                    return chain.filter(modifiedExchange);
                 }
+
+                // Caso o token não seja um JWT, passa a requisição original
                 return chain.filter(exchange);
-            })
-            .switchIfEmpty(chain.filter(exchange));
+            });
     }
 }
