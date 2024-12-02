a422da0444e07b49f5e3b6f7579396cd9fa423b3
martimsantos <santos.martim@ua.pt>
2024-12-02 18:43:04 +0000

    data collector almost done

--- datacollector/src/main/java/ies/tracktails/datacollector/Consumer.java
+++ datacollector/src/main/java/ies/tracktails/datacollector/Consumer.java
@@ -6,6 +6,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ies.tracktails.animalsDataCore.dtos.AnimalDataDTO;
 import ies.tracktails.animalsDataCore.services.AnimalDataService;
+import java.util.Optional;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
@@ -14,17 +15,31 @@ public class Consumer {
     @Autowired
     private AnimalDataService animalDataService;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public AnimalDataDTO convertToAnimalDataDTO(DataDTO data) {
+        AnimalDataDTO animalDataDTO = new AnimalDataDTO();
+        animalDataDTO.setAnimalId(data.getDeviceId());
+        animalDataDTO.setLatitude(Optional.ofNullable(data.getLatitude()));
+        animalDataDTO.setLongitude(Optional.ofNullable(data.getLongitude()));
+        animalDataDTO.setSpeed(Optional.ofNullable(data.getSpeed()));
+        animalDataDTO.setHeartRate(Optional.ofNullable(data.getBpm()));
+        return animalDataDTO;
+    }
+
     @KafkaListener(topics = "animal_tracking_topic", groupId = "kafka-listener-group")
     public void listen(String message) {
-        ObjectMapper mapper = new ObjectMapper();
         try {
-            DataDTO animalData = mapper.readValue(message, DataDTO.class);
-            animalDataService.writeAnimalData(animalData);
+            DataDTO data = objectMapper.readValue(message, DataDTO.class);
+            AnimalDataDTO animalDataDTO = convertToAnimalDataDTO(data);
+            animalDataService.writeAnimalData(animalDataDTO);
         } catch (Exception e) {
             System.err.println("Error processing message: " + e.getMessage());
         }
     }
 }
+
 
 
 
--- datacollector/src/main/java/ies/tracktails/datacollector/DataDTO.java
+++ datacollector/src/main/java/ies/tracktails/datacollector/DataDTO.java
@@ -1,33 +1,24 @@
 package ies.tracktails.datacollector;
 
 public class DataDTO {
-    private long timestamp;
+
     private String deviceId;
-    private String state;
     private double speed;
     private double bpm;
+    private double respiratory_rate;
     private double latitude;
     private double longitude;
 
     public DataDTO() {
     }
 
-    public DataDTO(long timestamp, String deviceId, String state, double speed, double bpm, double latitude, double longitude) {
-        this.timestamp = timestamp;
+    public DataDTO(String deviceId, double speed, double bpm, double respiratory_rate, double latitude, double longitude) {
         this.deviceId = deviceId;
-        this.state = state;
         this.speed = speed;
         this.bpm = bpm;
+        this.respiratory_rate = respiratory_rate;
         this.latitude = latitude;
         this.longitude = longitude;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
     }
 
     public String getDeviceId() {
@@ -36,14 +27,6 @@ public class DataDTO {
 
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
     }
 
     public double getSpeed() {
@@ -60,6 +43,14 @@ public class DataDTO {
 
     public void setBpm(double bpm) {
         this.bpm = bpm;
+    }
+
+    public double getRespiratory_rate() {
+        return respiratory_rate;
+    }
+
+    public void setRespiratory_rate(double respiratory_rate) {
+        this.respiratory_rate = respiratory_rate;
     }
 
     public double getLatitude() {
@@ -81,13 +72,13 @@ public class DataDTO {
     @Override
     public String toString() {
         return "DataDTO{" +
-                "timestamp=" + timestamp +
-                ", deviceId='" + deviceId + '\'' +
-                ", state='" + state + '\'' +
+                "deviceId='" + deviceId + '\'' +
                 ", speed=" + speed +
                 ", bpm=" + bpm +
+                ", respiratory_rate=" + respiratory_rate +
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
                 '}';
     }
+
 }
